51b7a5825566a857adb107287fdaf243ec1f6087
venkadesh004 <venkadeshdeveloper@gmail.com>
2023-09-04 03:09:43 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      chats: [
+        {
+          user: "bot",
+          chat: "How can I help you",
+        },
+      ],
+    };
+  }
+
+  render() {
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      console.log(event.target[0].value);
+
+      if (event.target[0].value !== "") {
+        var l = this.state.chats;
+
+        l.push({ user: "client", chat: event.target[0].value });
+
+        this.setState({
+          chats: l,
+        });
+
+        console.log(chats);
+
+        await axios
+          .get("http://127.0.0.1:5000/" + event.target[0].value)
+          .then((output) => {
+            var newl = this.state.chats;
+
+            output.data.response.forEach((element) => {
+              newl.push({ user: "bot", chat: element });
+            });
+
+            this.setState({
+              chats: newl,
+            });
+          });
+
+        event.target[0].value = "";
+      }
+    };
+
+    var chats = this.state.chats.map((value, index) => {
+      return (
+        <div className={`${value.user} chats`}>
+          <p>{value.chat}</p>
+        </div>
+      );
+    });
+
+    return (
+      <div className="App">
+        <h1>Medical chat bot</h1>
+        <div className="maincontainer">
+          <div className="chat-window">{chats}</div>
+          <div className="inputs">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Enter the problem you are currently facing"
+              ></input>
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
